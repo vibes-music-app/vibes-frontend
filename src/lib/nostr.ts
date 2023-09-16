1879247721b000dea2d672f1ad1f8ded9f5367a8
zen <zen@zenen.space>
2023-09-16 13:52:05 -0400

    stash

--- src/lib/nostr.ts
+++ src/lib/nostr.ts
@@ -131,9 +131,12 @@ interface PostsFilter {
 export const getPosts = async (
     relay: Relay,
     author: string = "",
-    limit = 0
+    limit = 0,
+    delta = 0
 ) => {
     let filter: PostsFilter = { kinds: ["song"] };
+export const getPosts = async (relay: Relay, author = "", limit = 0, delta = 0) => {
+    let filter = { kinds: [1] }
     if (author) {
         filter["author"] = [author];
     }
@@ -142,44 +145,50 @@ export const getPosts = async (
         filter["limit"] = limit;
     }
 
-    const posts = await getEvents(relay, [filter]);
-    return posts;
-};
+    // delta is a measure of how far back to pull posts (in seconds)
+    if (delta) {
+        delta = delta * 1000
+        const now = new Date()
+        filter['since'] = Math.floor(now.getTime()/1000) - delta
+    }
+
+    const posts = await getEvents(relay, [filter])
+    return posts
+}
 
 export const getCollections = async (relay: Relay) => {
     const posts = await getEvents(relay, [{ kinds: [2] }]);
     return posts;
 };
 
-// DEPRECATED
-export const subscribe = async (relay: Relay) => {
-    // Example subscription to a relay
+export const latestPosts = async(relay: Relay, latest = 10) => {
+    // This is a block for getting the n latest posts (more or less)
+    let found = 0
+    let posts = []
+    let default_delta = 60 * 60 // Start with 1 hour
 
-    let sub = relay.sub([
-        {
-            kinds: [1],
-        },
-    ]);
+    while (found < latest) {
+        // This is a base case. If we have found some results and the 
+        // latest iteration did not find more, we return what we have.
 
-    sub.on("event", (event) => {
-        console.log("new relay event!", event);
-    });
-};
+        if (found && posts.length == found) {
+            break
+        }
 
-export const subscribeToAuthor = async (relay: Relay, author: string) => {
-    // Example subscription to an author and publishing of an event
+        console.log('have ', found, 'posts, need', latest)
+        posts = await getPosts(relay, "", latest, default_delta);
+        default_delta *= 10 // Multiply by 10 each iteration
+        found = posts.length
+    }
 
-    let sub = relay.sub([
-        {
-            kinds: [1],
-            authors: [author],
-        },
-    ]);
+    return posts
+}
 
-    sub.on("event", (event) => {
-        console.log("got this eventaroo", event);
-    });
-};
+export const getEvent = async (relay: Relay, id: string) => {
+    const event = await getEvents(relay, [{ ids: [id] }])
+    return event
+}
+
 
 export const publishEvent = async (
     relay: Relay,
