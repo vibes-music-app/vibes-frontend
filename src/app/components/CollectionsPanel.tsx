93db96442a165fcaabdada55c50ea5838ba3448a
Christpher Oka <chriskroka@gmail.com>
2023-09-16 06:01:50 -0400

    added left collections sidebar

--- src/app/components/CollectionsPanel.tsx
+++ src/app/components/CollectionsPanel.tsx
@@ -7,8 +7,8 @@ export default async function CollectionsPanel() {
 
     return (
         <div>
-            <div className="bg-black flex gap-3 px-3 py-1 ">
-                <h2 className="text-white  text-2xl font-bold">Collections </h2>
+            <div className="bg-black flex gap-3 px-3 py-2 ">
+                <h2 className="text-white text-2xl font-bold">Collections </h2>
                 <div className="flex items-center justify-center">
                     <Image
                         src="/collections_icon.png"
@@ -20,17 +20,24 @@ export default async function CollectionsPanel() {
             </div>
             {collectionNames.map((collectionName) => {
                 return (
-                    <div className="border-b px-3 py-1">
+                    <div className="border-b px-3 py-2">
                         <Link href={`/collections/${collectionName.name}`}>
                             {collectionName.name}
                         </Link>
                     </div>
                 );
             })}
-            <div className="px-3 py-1">
+            <div className="flex gap-3 px-3 py-2">
                 {" "}
                 <div>New collection</div>
-                <div><Image src="" width="25" height="25"/></div>
+                <div className="flex items-center justify-center">
+                    <Image
+                        src="/plus_icon.png"
+                        width="25"
+                        height="25"
+                        alt="plus icon"
+                    />
+                </div>
             </div>
         </div>
     );
